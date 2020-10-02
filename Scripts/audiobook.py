32cd117080d902dd692c37ae523dd2fd7a1aaeea
ravi <rkgeekoftheweek@gmail.com>
2020-10-02 16:59:34 +0530

    ADDED : PDF to Audio book script

---
+++ Scripts/audiobook.py
@@ -0,0 +1,30 @@
+# github: rkgeekoftheweek
+
+#install required packages
+import pyttsx3
+import PyPDF2
+
+
+def read_book(bookName):
+    pdfBook = open(bookName, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfBook)
+    return pdfReader
+
+
+def listen_book(reader,pageNumber):
+    speaker = pyttsx3.init()
+    page = reader.getPage(pageNumber)
+    text = page.extractText()
+    speaker.say(text)
+    speaker.runAndWait()
+
+
+if __name__ == "__main__":
+
+    #change bookname and page number here
+    bookName ="alchemist.pdf"
+    pageNumber = 7
+    
+   reader = read_book(bookName)
+   listen_book(reader,pageNumber)
+    
