7dce0fb709fa547eaa5d26458a74c96208f465b3
Restyled.io <commits@restyled.io>
2020-10-02 02:17:55 +0000

    Restyled by black

--- Scripts/articletts.py
+++ Scripts/articletts.py
@@ -6,24 +6,23 @@ from time import sleep
 from playsound import playsound
 
 another_article = ""
-while another_article != 'n':
-    system('clear')
+while another_article != "n":
+    system("clear")
     url = input("Enter article url: ")
     page = requests.get(url).text
     soup = BeautifulSoup(page, features="lxml")
 
-    title = soup.find('h1').get_text()
+    title = soup.find("h1").get_text()
 
-    p_tags = soup.find_all('p')
+    p_tags = soup.find_all("p")
     p_text = [tag.get_text().strip() for tag in p_tags]
-    sentence_list = [sentence for sentence in p_text if not '\n' in sentence]
-    sentence_list_final = [
-        sentence for sentence in sentence_list if '.' in sentence]
+    sentence_list = [sentence for sentence in p_text if not "\n" in sentence]
+    sentence_list_final = [sentence for sentence in sentence_list if "." in sentence]
 
-    article_text = ' '.join(sentence_list_final)
+    article_text = " ".join(sentence_list_final)
 
-    tts = gTTS(article_text, lang='en')
-    tts.save('article.mp3')
+    tts = gTTS(article_text, lang="en")
+    tts.save("article.mp3")
     playsound("article.mp3")
 
     another_article = input("Wold you like to play another article? [y/n]: ")
