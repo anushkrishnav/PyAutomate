bb5d332a2f2bae5e506ccb4ce7c5990b4be39ea3
Restyled.io <commits@restyled.io>
2020-10-02 20:46:33 +0000

    Restyled by autopep8

--- sripts/Email_Scrapper.py
+++ sripts/Email_Scrapper.py
@@ -6,25 +6,25 @@ from bs4 import BeautifulSoup
 import pandas as pd
 from google.colab import files
 
-original_url = input("Enter the website url: ") 
+original_url = input("Enter the website url: ")
 
-unscraped = deque([original_url])  
+unscraped = deque([original_url])
 
-scraped = set()  
+scraped = set()
 
-emails = set()  
+emails = set()
 
 while len(unscraped):
-    url = unscraped.popleft()  
+    url = unscraped.popleft()
     scraped.add(url)
 
     parts = urlsplit(url)
 
     base_url = "{0.scheme}://{0.netloc}".format(parts)
     if '/' in parts.path:
-      path = url[:url.rfind('/')+1]
+        path = url[:url.rfind('/')+1]
     else:
-      path = url
+        path = url
 
     print("Crawling URL %s" % url)
     try:
@@ -32,28 +32,29 @@ while len(unscraped):
     except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
         continue
 
-    new_emails = set(re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.com", response.text, re.I))
-    emails.update(new_emails) 
+    new_emails = set(re.findall(
+        r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.com", response.text, re.I))
+    emails.update(new_emails)
 
     soup = BeautifulSoup(response.text, 'lxml')
 
     for anchor in soup.find_all("a"):
-      if "href" in anchor.attrs:
-        link = anchor.attrs["href"]
-      else:
-        link = ''
+        if "href" in anchor.attrs:
+            link = anchor.attrs["href"]
+        else:
+            link = ''
 
-        if link.startswith('/'):
-            link = base_url + link
+            if link.startswith('/'):
+                link = base_url + link
 
-        elif not link.startswith('http'):
-            link = path + link
+            elif not link.startswith('http'):
+                link = path + link
 
-        if not link.endswith(".gz"):
-          if not link in unscraped and not link in scraped:
-              unscraped.append(link)
+            if not link.endswith(".gz"):
+                if not link in unscraped and not link in scraped:
+                    unscraped.append(link)
 
 df = pd.DataFrame(emails, columns=["Email"])
 df.to_csv('email.csv', index=False)
 
-files.download("email.csv") 
+files.download("email.csv")
