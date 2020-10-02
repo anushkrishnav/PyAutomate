97447a0505ae63816698bd7302015e9131d1bb3e
Restyled.io <commits@restyled.io>
2020-10-02 20:46:34 +0000

    Restyled by black

--- sripts/Email_Scrapper.py
+++ sripts/Email_Scrapper.py
@@ -21,8 +21,8 @@ while len(unscraped):
     parts = urlsplit(url)
 
     base_url = "{0.scheme}://{0.netloc}".format(parts)
-    if '/' in parts.path:
-        path = url[:url.rfind('/')+1]
+    if "/" in parts.path:
+        path = url[: url.rfind("/") + 1]
     else:
         path = url
 
@@ -32,22 +32,23 @@ while len(unscraped):
     except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError):
         continue
 
-    new_emails = set(re.findall(
-        r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.com", response.text, re.I))
+    new_emails = set(
+        re.findall(r"[a-z0-9\.\-+_]+@[a-z0-9\.\-+_]+\.com", response.text, re.I)
+    )
     emails.update(new_emails)
 
-    soup = BeautifulSoup(response.text, 'lxml')
+    soup = BeautifulSoup(response.text, "lxml")
 
     for anchor in soup.find_all("a"):
         if "href" in anchor.attrs:
             link = anchor.attrs["href"]
         else:
-            link = ''
+            link = ""
 
-            if link.startswith('/'):
+            if link.startswith("/"):
                 link = base_url + link
 
-            elif not link.startswith('http'):
+            elif not link.startswith("http"):
                 link = path + link
 
             if not link.endswith(".gz"):
@@ -55,6 +56,6 @@ while len(unscraped):
                     unscraped.append(link)
 
 df = pd.DataFrame(emails, columns=["Email"])
-df.to_csv('email.csv', index=False)
+df.to_csv("email.csv", index=False)
 
 files.download("email.csv")
