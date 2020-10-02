fdaa0eea28d39e830f8b64370e3497c4594688ea
Restyled.io <commits@restyled.io>
2020-10-02 20:25:00 +0000

    Restyled by black

--- stocks_track/stocks_data.py
+++ stocks_track/stocks_data.py
@@ -7,6 +7,4 @@ from nsepy import get_history
 # get data of sbin stock
 
 
-sbin = get_history(symbol='SBIN',
-                   start=date(2020, 8, 10),
-                   end=date(2020, 8, 16))
+sbin = get_history(symbol="SBIN", start=date(2020, 8, 10), end=date(2020, 8, 16))
